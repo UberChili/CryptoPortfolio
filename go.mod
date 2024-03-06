77a43ccb7ec7e5af5e3daa6c34159b319bb37a39
Andrés <nunezlopez.ja@gmail.com>
2024-03-05 20:13:13 -0600

    modified .gitignore. Removed some files from tracking

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module main
 
 go 1.22.0
 
-require golang.org/x/crypto v0.21.0 // indirect
+require (
+	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	golang.org/x/crypto v0.21.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,18 +1,17 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	"golang.org/x/crypto/bcrypt"
 	"html/template"
 	"log"
 	"net/http"
-	// "golang.org/x/crypto/bcrypt"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
-type Session struct {
-	Username string
-	id       string
-	Logged   bool
-}
+var db *sql.DB
 
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.ParseFiles("./templates/layout.html", "./templates/index.html"))
@@ -67,10 +66,92 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 			fmt.Println("Must provide username and password.")
 			return
 		}
-		fmt.Println(name, pass)
-		fmt.Println("Got to make it look like I'm doing work or else my dad will have me doing some stupid shit")
+		// fmt.Println(name, pass)
 	} else { // User reached route via GET (as by clicking a link or via redirect)
 		tmpl := template.Must(template.ParseFiles("./templates/layout.html", "./templates/login.html"))
+		tmpl.Execute(w, nil)
+	}
+}
+
+// Function to handle the register page
+func registerHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" { // User reached route via POST (as by submitting a form via POST)
+		// Ensure user provided username and password
+		err := r.ParseForm()
+		if err != nil {
+			fmt.Println("Error parsing!")
+			return
+		}
+		// Get values
+		username := r.PostFormValue("username")
+		pass := r.PostFormValue("password")
+		confirmation := r.PostFormValue("confirmation")
+		fmt.Println(username, pass, confirmation)
+
+		// Check for correct value criteria
+		var err_message string
+
+		if username == "" {
+			fmt.Println("Username field empty")
+			err_message = "Must provide a username"
+		} else if pass == "" || confirmation == "" {
+			fmt.Println("Password or confirmation are empty")
+			err_message = "Must provide a password and a password confirmation"
+		} else if pass != confirmation {
+			fmt.Println("Different password and confirmation")
+			err_message = "Password and password confirmation are not the same"
+		}
+		if err_message != "" {
+			var Message = map[string]string{"Message": err_message}
+			tmpl := template.Must(template.ParseFiles("./templates/layout.html", "./templates/register.html"))
+			tmpl.Execute(w, Message)
+		}
+		// Check if user already exists
+		stmt := "SELECT id FROM users WHERE username = ?"
+		row := db.QueryRow(stmt, username)
+		var id string
+		err_scan := row.Scan(&id)
+		if err_scan != sql.ErrNoRows {
+			fmt.Println("User already exists")
+			err_message = "User already exists"
+			var Message = map[string]string{"Message": err_message}
+			tmpl := template.Must(template.ParseFiles("./templates/layout.html", "./templates/register.html"))
+			tmpl.Execute(w, Message)
+			return
+		}
+		// Create hash from password
+		var hash []byte
+		hash, err = bcrypt.GenerateFromPassword([]byte(pass), bcrypt.DefaultCost)
+		if err != nil {
+			fmt.Println("bcrypt err: ", err)
+			err_message = err.Error()
+			var Message = map[string]string{"Message": err_message}
+			tmpl := template.Must(template.ParseFiles("./templates/layout.html", "./templates/register.html"))
+			tmpl.Execute(w, Message)
+			return
+		}
+		// Insert new user into table
+		var insertStmt *sql.Stmt
+		insertStmt, err = db.Prepare("INSERT INTO users (username, hash) VALUES (?, ?)")
+		if err != nil {
+			fmt.Println("Error preparing insert statement")
+			return
+		}
+		defer insertStmt.Close()
+		var result sql.Result
+		result, err = insertStmt.Exec(username, hash)
+		rowsAff, _ := result.RowsAffected()
+		lastIns, _ := result.LastInsertId()
+		fmt.Println("RowsAff: ", rowsAff)
+		fmt.Println("lastIns: ", lastIns)
+		fmt.Println("err: ", err)
+		if err != nil {
+			fmt.Println("Error inserting new user to table")
+			return
+		}
+
+	} else { // User reached route via GET (as by clicking a link or via redirect)
+		tmpl := template.Must(template.ParseFiles("./templates/layout.html", "./templates/register.html"))
 		tmpl.Execute(w, nil)
 	}
 }
@@ -79,10 +160,35 @@ func main() {
 	http.Handle("/static/",
 		http.StripPrefix("/static/", http.FileServer(http.Dir("static"))))
 
+	var err error
+	db, err = sql.Open("sqlite3", "finance.db")
+	if err != nil {
+		panic(err.Error())
+	}
+	defer db.Close()
+
+	// rows, err := db.Query("SELECT * FROM users")
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// for rows.Next() {
+	// 	var id int
+	// 	var username string
+	// 	var hash string
+	// 	var cash float64
+	//
+	// 	err = rows.Scan(&id, &username, &hash, &cash)
+	// 	if err != nil {
+	// 		log.Fatal(err)
+	// 	}
+	// 	fmt.Printf("%d, %s, %s, %f\n", id, username, hash, cash)
+	// }
+
 	// Handle functions
 	http.HandleFunc("/", indexHandler)
 	http.HandleFunc("/quote/", quoteHandler)
 	http.HandleFunc("/login/", loginHandler)
+	http.HandleFunc("/register/", registerHandler)
 
 	log.Println("App running on 8000...")
 	log.Fatal(http.ListenAndServe(":8000", nil))
